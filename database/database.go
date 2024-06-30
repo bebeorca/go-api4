9c878bb0fa89dda2a2b99e08a3d1db67e770b13a
bebeorca <dimasrdyo@gmail.com>
2024-06-30 13:35:51 +0700

    aduhai

--- database/database.go
+++ database/database.go
@@ -3,7 +3,7 @@ package database
 import (
 	"fmt"
 	"log"
-	// "os"
+	"os"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -19,16 +19,16 @@ func DatabaseInit() {
 	}
 
 	// Access environment variables
-	// dbUser := os.Getenv("DB_USER")
-	// dbPassword := os.Getenv("DB_PASSWORD")
-	// dbHost := os.Getenv("DB_HOST")
-	// dbPort := os.Getenv("DB_PORT")
-	// dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 	
 	var err error
 
-	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
-	DB, err = gorm.Open(mysql.Open("mysql://root:ghJFQtLGVejRgjxzrptyKaRteTnasPWW@(monorail.proxy.rlwy.net:54936)/railway"), &gorm.Config{})
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Failed to Connect to database...")
