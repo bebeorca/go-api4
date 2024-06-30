6aa45f225a404ade86b6ea58505db0da78a8d616
bebeorca <dimasrdyo@gmail.com>
2024-06-30 13:29:21 +0700

    aduhai

--- database/database.go
+++ database/database.go
@@ -28,7 +28,7 @@ func DatabaseInit() {
 	var err error
 
 	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
-	DB, err = gorm.Open(mysql.Open("mysql://root:ghJFQtLGVejRgjxzrptyKaRteTnasPWW@monorail.proxy.rlwy.net:54936/railway"), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open("mysql://root:ghJFQtLGVejRgjxzrptyKaRteTnasPWW@tcp(monorail.proxy.rlwy.net:54936)/railway"), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Failed to Connect to database...")
