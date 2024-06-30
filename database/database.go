6716e444df8ad8dc620d74325970c24c286851de
bebeorca <dimasrdyo@gmail.com>
2024-06-30 13:26:44 +0700

    aduhai

--- database/database.go
+++ database/database.go
@@ -3,36 +3,36 @@ package database
 import (
 	"fmt"
 	"log"
-	"os"
-   
+	// "os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"github.com/joho/godotenv"
-   )
-   
-   var DB *gorm.DB
-   
-   func DatabaseInit() {
-   
+)
+
+var DB *gorm.DB
+
+func DatabaseInit() {
+
 	if err := godotenv.Load(); err != nil {
-        log.Fatal("Error loading .env file")
-    }
-
-    // Access environment variables
-    dbHost := os.Getenv("DB_HOST")
-    dbPort := os.Getenv("DB_PORT")
-    dbUser := os.Getenv("DB_USER")
-    dbName := os.Getenv("DB_NAME")
-    dbPassword := os.Getenv("DB_PASSWORD")
-
-	var err error
-   
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-   
-	if err != nil {
-	 log.Fatal("Failed to Connect to database...")
+		log.Fatal("Error loading .env file")
 	}
-   
+
+	// Access environment variables
+	// dbUser := os.Getenv("DB_USER")
+	// dbPassword := os.Getenv("DB_PASSWORD")
+	// dbHost := os.Getenv("DB_HOST")
+	// dbPort := os.Getenv("DB_PORT")
+	// dbName := os.Getenv("DB_NAME")
+	
+	var err error
+
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	DB, err = gorm.Open(mysql.Open("mysql://root:ghJFQtLGVejRgjxzrptyKaRteTnasPWW@monorail.proxy.rlwy.net:54936/railway"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to Connect to database...")
+	}
+
 	fmt.Println("Connecting to database...")
 }
