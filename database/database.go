b1e7d5a1e27f397ebfb28239a54e9277199a8d7a
bebeorca <dimasrdyo@gmail.com>
2024-06-30 12:48:56 +0700

    apdet

--- database/database.go
+++ database/database.go
@@ -3,18 +3,30 @@ package database
 import (
 	"fmt"
 	"log"
+	"os"
    
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"github.com/joho/godotenv"
    )
    
    var DB *gorm.DB
    
    func DatabaseInit() {
    
+	if err := godotenv.Load(); err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    // Access environment variables
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+    dbUser := os.Getenv("DB_USER")
+    dbName := os.Getenv("DB_NAME")
+
 	var err error
    
-	dsn := "root:@tcp(127.0.0.1:3306)/dummydb"
+	dsn := fmt.Sprintf("%s:@tcp(%s:%s)/%s", dbUser, dbHost, dbPort, dbName)
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
    
 	if err != nil {
