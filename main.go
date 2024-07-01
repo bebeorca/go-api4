29d65ef9a3d547d515026643c7c17498660ebad8
bebeorca <dimasrdyo@gmail.com>
2024-07-01 16:31:17 +0700

    error handling

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ func main(){
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-        AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
         AllowOrigins:     "*",
+        AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
         AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
     }))
 
