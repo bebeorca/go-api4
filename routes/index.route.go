dcb3aca58a88d74e20d53be6931233aad08eb829
bebeorca <dimasrdyo@gmail.com>
2024-07-04 07:48:46 +0700

    update permit

--- routes/index.route.go
+++ routes/index.route.go
@@ -1,12 +1,13 @@
 package routes
 
 import (
+	"github.com/bebeorca/go-api4/config"
 	"github.com/bebeorca/go-api4/handler"
 	"github.com/gofiber/fiber/v2"
 )
 
 func RouteInit(r *fiber.App){
-	r.Static("/images", "./public/uploads")
+	r.Static("/images", config.ProjectRootPath+"/public/uploads")
 	r.Get("/", handler.UserHandlerRead)
 	r.Post("/register", handler.UserHandlerCreate)
 	r.Post("/upload", handler.UploadPost)
