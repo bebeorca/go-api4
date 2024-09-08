d13b75fd8cce567c0bf908fef85e936867048659
bebeorca <dimasrdyo@gmail.com>
2024-09-08 13:17:48 +0700

    tes shutdown

--- handler/shutdown.handler.go
+++ handler/shutdown.handler.go
@@ -1,8 +1,23 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	// "net/http"
+	"os/exec"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Shutdown(c *fiber.Ctx) error {
+
+	err := exec.Command("shutdown", "/s", "/t", "1").Run()
+
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"status":  500,
+			"message": "Shutdown request failed.",
+		})
+	}
+
 	return c.Status(200).JSON(fiber.Map{
 		"status": 200,
 		"message": "Shutdown request recieved.",
